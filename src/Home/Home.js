7eb8d53f6bb8061ea48a07f225d78464b1c49935
unknown <megakuul@gmx.ch>
2023-03-08 22:58:51 +0100

    Added Animated boxes + Intersection Observer

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,12 +1,27 @@
-console.log("Hallo");
-
 $(function(){
-    console.log("Hallo2");
-    $window = $(window);
-    $window.scroll(function(){
-      var scrollTop = $window.scrollTop();
-      $this = $('#welcome-main')
-      val = - scrollTop / 2;
-        $this.css('transform', 'translateY(' + val + 'px)');
-    });
+  $window = $(window);
+  $window.scroll(function(){
+    var scrollTop = $window.scrollTop();
+    $maincontainer = $(".main-container")
+    val = - scrollTop / 6;
+      $maincontainer.css("transform", 'translateY(' + val + 'px)');
+
+    $scrollr = $(".scrollr")
+    val = - scrollTop / 4;
+      $scrollr.css("transform", 'translateY(' + val + 'px)');
+  });
 })
+
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    } else {
+      entry.target.classList.remove("show");
+    }
+  });
+});
+
+const hiddenElements = document.querySelectorAll(".hidden");
+hiddenElements.forEach((element) => observer.observe(element));
